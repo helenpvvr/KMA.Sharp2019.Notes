38b7a4e09993e5d533a16592d3e9fb08cac3d580
Olena <olena.pyvovar1234@gmail.com>
2019-10-31 11:49:35 +0200

    Add DBModels project

---
+++ DBModels/Note.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.Sharp2019.Notes.MoreThanNotes.DBModels
+{
+    public class Note
+    {
+        #region Fields
+        private Guid _guid;
+        private string _title;
+        private string _noteText;
+        private readonly DateTime? _createdDateTime;
+        private DateTime? _editedDateTime;
+        private Guid _userGuid;
+        private User _user;
+        #endregion
+
+        #region Properties
+
+        public Guid Guid
+        {
+            get { return _guid; }
+            private set { _guid = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string NoteText {
+            get { return _noteText; }
+            set { _noteText = value; }
+        }
+        public DateTime? CreatedDateTime
+        {
+            get { return _createdDateTime; }
+        }
+        public DateTime? EditedDateTime
+        {
+            get { return _editedDateTime;}
+            set { _editedDateTime = value; }
+        }
+        public Guid UserGuid
+        {
+            get { return _userGuid; }
+            private set { _userGuid = value; }
+        }
+        public User User
+        {
+            get { return _user; }
+            private set { _user = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Note(string title, string noteText, DateTime? createdDateTime)
+        {
+            _guid = Guid.NewGuid();
+            _title = title;
+            _noteText = noteText;
+            _createdDateTime = createdDateTime;
+            _editedDateTime = createdDateTime;
+        }
+
+        public Note() : this("", "", DateTime.Now)
+        {
+
+        }
+        #endregion
+
+        #region EntityFrameworkConfiguration
+        public class NoteEntityConfiguration : EntityTypeConfiguration<Note>
+        {
+            public NoteEntityConfiguration()
+            {
+                ToTable("Notes");
+                HasKey(n => n.Guid);
+
+                Property(p => p.Guid)
+                    .HasColumnName("Guid")
+                    .IsRequired();
+                Property(p => p.Title)
+                    .HasColumnName("Title")
+                    .IsRequired();
+                Property(p => p.NoteText)
+                    .HasColumnName("NoteText")
+                    .IsRequired();
+                Property(p => p.CreatedDateTime)
+                    .HasColumnName("CreatedDateTime")
+                    .IsRequired();
+                Property(p => p.EditedDateTime)
+                    .HasColumnName("EditedDateTime")
+                    .IsRequired();
+            }
+        }
+        #endregion
+    }
+}
---
+++ DBModels/User.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+
+namespace KMA.Sharp2019.Notes.MoreThanNotes.DBModels
+{
+    public class User
+    {
+        #region Fields
+        private Guid _guid;
+        private string _login;
+        private string _email;
+        private string _password;
+        private List<Note> _notes;
+        #endregion
+
+        #region Properties
+        public Guid Guid
+        {
+            get { return _guid; }
+            private set { _guid = value; }
+        }
+        public string Login
+        {
+            get { return _login; }
+            private set { _login = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            private set { _email = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public List<Note> Notes
+        {
+            get { return _notes; }
+            set { _notes = value; }
+        }
+        #endregion
+
+        #region Constructor
+
+        public User(string login, string email, string password)
+        {
+            _guid = Guid.NewGuid();
+            _login = login;
+            _email = email;
+            SetPassword(password);
+            _notes = new List<Note>();
+        }
+
+        public User()
+        {
+
+        }
+        #endregion
+
+        private void SetPassword(string password)
+        {
+            //TODO Add encription
+            _password = password;
+        }
+
+        internal bool CheckPassword(string password)
+        {
+            //TODO Compare encrypted passwords
+            return _password == password;
+        }
+
+        public override string ToString()
+        {
+            return Login;
+        }
+
+        public Note GetNoteByGuid(Guid guid)
+        {
+            return _notes.FirstOrDefault(n => n.Guid == guid);
+        }
+
+        public void AddNote(Note note)
+        {
+            _notes.Add(note);
+        }
+
+        public void RemoveNote(Note note)
+        {
+            _notes.RemoveAll(n => n.Guid == note.Guid);
+        }
+
+        #region EntityConfiguration
+
+        public class UserEntityConfiguration : EntityTypeConfiguration<User>
+        {
+            public UserEntityConfiguration()
+            {
+                ToTable("Users");
+                HasKey(u => u.Guid);
+
+                Property(u => u.Guid).HasColumnName("Guid").IsRequired();
+                Property(u => u.Login).HasColumnName("Login").IsRequired();
+                Property(u => u.Email).HasColumnName("Email").IsRequired();
+                Property(u => u.Password).HasColumnName("Password").IsRequired();
+            }
+        }
+
+        #endregion
+    }
+}
