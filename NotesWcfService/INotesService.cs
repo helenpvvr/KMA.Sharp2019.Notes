63da4e1a3d03271e47930822428f1552ec0736e5
Alexandra <a.r.chumachenko@gmail.com>
2019-10-31 13:22:31 +0200

    Connect Server

---
+++ NotesWcfService/INotesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace NotesWcfService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "INotesService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface INotesService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
